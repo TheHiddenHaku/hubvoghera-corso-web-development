911a38abdd61190174735af7e1366d869c1b3503
TheHiddenHaku <haku78@gmail.com>
2014-11-27 13:57:45 +0000

    klein => router name change

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
     
     require_once "config/site_config.php";
     
-    $klein->respond('GET', '/', function () {
+    $router->respond('GET', '/', function () {
         echo "<h1>Benvenuti al corso di Web Development</h1>";
     });
 
-    $klein->respond('/hello/[:name]', function ($request) {
+    $router->respond('/hello/[:name]', function ($request) {
         $controller = new DefaultController();
         $controller->hello($request->name);
     });
 
-    $klein->dispatch();
+    $router->dispatch();
